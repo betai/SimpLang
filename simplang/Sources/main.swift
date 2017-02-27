7f8d897d03934e154bdec9f9c88deb6418100adb
Betai Koffi <betai@xamarin.com>
2017-02-27 13:06:43 -0800

    make it easier to run tests

--- simplang/Sources/main.swift
+++ simplang/Sources/main.swift
@@ -1,21 +1,38 @@
 import Darwin
 
-if CommandLine.arguments.count != 2 {
-    print("Usage: simplang foo.sl")
-    print("Does not support multiple files yet")
-    exit(1)
-} else {
-    let sourcePath = CommandLine.arguments[1]
-    var scanner = Scanner(sourcePath: sourcePath)
-    // scanner shit
-//    for token in scanner.tokens {
-//        print("\(token.scannerDescription)")
-//    }
 
-    // parser shit
+func usage() {
+    print("Usage:")
+    print(" simplang --scan foo.sl")
+    print(" simplang --parse foo.sl")
+    print(" simplang --interpret foo.sl")
+    print("Does not support multiple files yet")
+    exit(0)
+}
+
+if CommandLine.arguments.count != 3 {
+    usage()
+} else {
+    let option = CommandLine.arguments[1]
+    let sourcePath = CommandLine.arguments[2]
+
+    var scanner = Scanner(sourcePath: sourcePath)
+
+    if option == "--scan" {
+        for token in scanner.tokens {
+            print("\(token.scannerDescription)")
+        }
+        exit(0)
+    }
+    
     var parser = Parser(tokens: scanner.tokens)
-    print("\(parser.syntaxTreeString)")
-//    for node in parser.nodes {
-//        print("\(node.eval())")
-//    }
+
+    if option == "--parse" {
+        print("\(parser.syntaxTreeString)")
+    } else if option == "--interpret" {
+        for node in parser.nodes {
+            print("\(node.eval())")
+        }
+    }
+
 }

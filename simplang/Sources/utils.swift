67c25d1a6048b7ba638fd197669338b8e14a6a20
Betai Koffi <betai@xamarin.com>
2017-02-26 17:36:08 -0800

    stack implementation

---
+++ simplang/Sources/utils.swift
@@ -0,0 +1,28 @@
+class Stack<T> {
+
+    public var count : Int {
+        get {
+            return data.count
+        }
+    }
+    private var data : [T] = [T]()
+
+    init() {
+    }
+
+    public func Push(_ elem: T) {
+        data = [elem] + data
+    }
+
+    public func Pop() -> T? {
+        if let first = data.first {
+            data.removeFirst()
+            return first
+        }
+        return nil
+    }
+
+    public func Peek() -> T? {
+        return data.first
+    }
+}
